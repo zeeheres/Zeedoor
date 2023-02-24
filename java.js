d92db8e2a5e5e0a6274b1dbbaef70aecb7ecfd9c
Zee <123334173+zeeheres@users.noreply.github.com>
2023-02-24 17:25:09 +0500

    Add files via upload
    Hey, what's goin on. Picked this project because it covers the basic what if JS

---
+++ java.js
@@ -0,0 +1,12 @@
+function pressed(){
+   var text= document.getElementById ("inp").value;
+   if (text==="red"){
+      document.getElementById("header").style.color = "red";     
+   }  
+   else if{
+      document.getElementById("header").style.color = "Green";
+   }
+ else {
+   document.getElementById("header").stytle.color ="white"
+ }
+} 
